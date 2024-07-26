dc11bb262f75fe339e31b3ed0c6a671fb7e67d99
Johanan <117050032+Johanan-Dream@users.noreply.github.com>
2024-07-26 12:19:30 +0900

    Title: 입력받아 계산 5, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240726/입력받아 계산 5/input-calculate-5.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int result = printInt();
+        System.out.println(result);
+    }
+
+    public static int printInt(){
+        int result = 0;
+
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+        result = a + b;
+
+        return result;
+    }
+}
