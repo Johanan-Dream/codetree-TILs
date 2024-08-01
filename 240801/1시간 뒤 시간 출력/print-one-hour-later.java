202328aa13c2e754eb168d3c80570488233d1dc6
Johanan <117050032+Johanan-Dream@users.noreply.github.com>
2024-08-01 11:26:09 +0900

    Title: 1시간 뒤 시간 출력, Time: 161ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240801/1시간 뒤 시간 출력/print-one-hour-later.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        sc.useDelimiter(":");
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+
+        h = h+1;
+
+        System.out.print(h+":"+m);
+    }
+}
